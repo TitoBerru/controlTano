a47efd3cccbc8b8c7d7167b0063723109b321bbb
Hector Berrutti <82546028+TitoBerru@users.noreply.github.com>
2024-08-06 14:36:36 -0300

    se agrego el logo del club

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-red-600 text-white p-4">
+    <header className="bg-red-600 text-white p-4 max-h-13" >
+      <img src="/logo192.png" alt="Escudo Italiano Rugby" className='size-16'/>
       <h1 className="text-2xl font-bold text-center">CLUB ITALIANO RUGBY - ENTRENAMIENTOS</h1>
     </header>
   );
